8da8825f765a5f1b3fc36c09587ab1df43234ebb
mas9311 <mas9311@gmail.com>
2019-04-23 15:12:43 -0600

    Fix some of the testing failures

--- test/create_party.js
+++ test/create_party.js
@@ -4,30 +4,21 @@ const web3 = new Web3(Web3.givenProvider);
 var ConsentContract = artifacts.require("ConsentContract.sol");
 
 contract('ConsentContract:createParty', function(accounts) {
-  let contract;
-
-  beforeEach('setup contract for each test', async function () {
-    // contract = await ConsentContract.new();
-    contract = await ConsentContract.deployed();
-  });
-
-  it("create new Profile for all tests to use", async function() {
-    await contract.createProfile("Alice", "Anderson", {gas: 100000, from: accounts[0]});
-  });
 
   /** Success-result tests */
 
-  it("should create a Party", async function() {
-    // contract = await contract.deployed();
-    await contract.createParty("AliceAndersonsParty_1a", {gas: 500000, from: accounts[0]});
+  it("should create a Party (1a)", async function() {
+    let contract = ConsentContract.deployed();
+
+    await contract.createProfile("Alice", "Anderson", {gas: 500000, from: accounts[0]});
+    await contract.createParty1A("AliceAndersonsParty_1a", {gas: 500000, from: accounts[0]});
   });
 
-  it("should create a Party", async function() {
-    // contract = await contract.deployed();
-    let num = (web3.utils.padLeft(web3.utils.numberToHex(2), 2, "0"));
-    console.log(num);
+  it("should create a Party with maxNumber of Guests (1b)", async function() {
+    let contract = ConsentContract.deployed();
 
-    await contract.createParty("AliceAndersonsParty_1b", num, {gas: 500000, from: accounts[0]});
+    await contract.createProfile("Alice", "Anderson", {gas: 500000, from: accounts[0]});
+    await contract.createParty1B("AliceAndersonsParty_1b", 2, {gas: 500000, from: accounts[0]});
   });
 
 
