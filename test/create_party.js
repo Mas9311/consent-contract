e94c78ae237379a9f8fae5b2c696aeb490776d59
Michael Smith <mas9311@gmail.com>
2019-04-22 21:53:00 -0600

    Added a new test and made a commented out section. This is not ideal, but if you want to negative-test (test to make sure it fails),   then comment out the test cases (one at a time) and run 'truffle test'. Every one of the commented out tests should fail.   - If they do not fail, there there is a bug.

--- test/create_party.js
+++ test/create_party.js
@@ -1,11 +1,24 @@
-//
-// var ConsentContract = artifacts.require("ConsentContract.sol");
-//
-// contract('ConsentContract:createParty', function(accounts) {
-//   it("should create a Profile", async function(done) {
-//     var create_party = await ConsentContract.deployed();
-//
-//     assert.isTrue(true);
-//     done();
-//   });
-// });
+
+var ConsentContract = artifacts.require("ConsentContract.sol");
+
+contract('ConsentContract:createParty', function(accounts) {
+
+  it("should create a Party", async function() {
+    let contract = await ConsentContract.deployed();
+
+    await contract.createProfile("Alice", "Anderson", {gas: 100000, from: accounts[0]});
+    await contract.createParty("AliceAndersonsParty", {gas: 500000, from: accounts[0]});
+  });
+
+  //Uncomment the following tests (one by one) and they should fail. If they don't, there is a bug!
+
+  // it("should not allow the creation of a Party with an existing name", async function() {
+  //   let contract = await ConsentContract.deployed();
+  //
+  //   await contract.createProfile("Alice", "Anderson", {gas: 100000, from: accounts[0]});
+  //   await contract.createParty("AliceAndersonsParty", {gas: 500000, from: accounts[0]});
+  //
+  //   await contract.createParty("AliceAndersonsParty", {gas: 500000, from: accounts[0]});
+  // });
+
+});
--- test/create_profile.js
+++ test/create_profile.js
@@ -3,21 +3,18 @@ const truffleAssert = require('truffle-assertions');
 var ConsentContract = artifacts.require("./ConsentContract.sol");
 
 contract('ConsentContract:createProfile', function(accounts) {
+
   it("should create a Profile", async function () {
     let contract = await ConsentContract.deployed();
     await contract.createProfile("Alice", "Anderson", {gas: 100000, from: accounts[0]});
-    assert(true);
-    console.log("Created profile");
   });
 
-  it("should not allow to create multitple Profiles", async function () {
-    let contract = await ConsentContract.deployed();
-    await contract.createProfile("Alice", "Anderson", {gas: 100000, from: accounts[0]});
-    await truffleAssert.fails(
-      contract.createProfile("Alice", "Anderson", {gas: 100000, from: accounts[0]}),
-      truffleAssert.ErrorType.REVERT,
-      "You have already created a profile"
-  );
-  });
+  //Uncomment the following test and it should fail. If it doesn't, there is a bug!
+
+  // it("should not allow a user to create multiple Profiles", async function () {
+  //   let contract = await ConsentContract.deployed();
+  //   await contract.createProfile("Alice", "Anderson", {gas: 100000, from: accounts[0]});
+  //   contract.createProfile("Someone", "Else", {gas: 100000, from: accounts[0]});
+  // });
 
 });
