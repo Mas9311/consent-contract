af4ce5dd41fd9ab08d140d571bbb2c041885f730
mas9311 <mas9311@gmail.com>
2019-04-23 15:19:25 -0600

    Added await to ConsentContract.deployed()

--- test/create_party.js
+++ test/create_party.js
@@ -1,6 +1,3 @@
-const Web3 = require("web3");
-const web3 = new Web3(Web3.givenProvider);
-
 var ConsentContract = artifacts.require("ConsentContract.sol");
 
 contract('ConsentContract:createParty', function(accounts) {
@@ -8,16 +5,14 @@ contract('ConsentContract:createParty', function(accounts) {
   /** Success-result tests */
 
   it("should create a Party (1a)", async function() {
-    let contract = ConsentContract.deployed();
-
-    await contract.createProfile("Alice", "Anderson", {gas: 500000, from: accounts[0]});
+    let contract = await ConsentContract.deployed();
+    await contract.createProfile("Alice", "Anderson", {gas: 100000, from: accounts[0]});
     await contract.createParty1A("AliceAndersonsParty_1a", {gas: 500000, from: accounts[0]});
   });
 
   it("should create a Party with maxNumber of Guests (1b)", async function() {
-    let contract = ConsentContract.deployed();
+    let contract = await ConsentContract.deployed();
 
-    await contract.createProfile("Alice", "Anderson", {gas: 500000, from: accounts[0]});
     await contract.createParty1B("AliceAndersonsParty_1b", 2, {gas: 500000, from: accounts[0]});
   });
 
