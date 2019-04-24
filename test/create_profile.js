8e67ba3ef06be269f01abc92604745016634d007
Evan Steiner <evanjs@washington-dc.cs.colostate.edu>
2019-04-24 09:24:19 -0600

    added tests to ensure that duplicate parties and profiles cant be created, or empty profiles/parties

--- test/create_profile.js
+++ test/create_profile.js
@@ -1,10 +1,31 @@
 var ConsentContract = artifacts.require("ConsentContract.sol");
+const assert = require("assert");
+
 
 contract('ConsentContract:createProfile', function(accounts) {
 
   it("should create a Profile", async function () {
     let contract = await ConsentContract.deployed();
     await contract.createProfile("Alice", "Anderson", {gas: 100000, from: accounts[0]});
+  });
+
+  it("should fail to create a Profile, already registered", async function () {
+    let contract = await ConsentContract.deployed();
+    try {
+      await contract.createProfile("Alice", "Anderson", {gas: 100000, from: accounts[0]});
+      assert(false)
+    } catch(err) {
+      assert(err)
+    }
+  });
+  it("should fail to create a Profile, empty name", async function () {
+    let contract = await ConsentContract.deployed();
+    try {
+      await contract.createProfile("", "Fail", {gas: 100000, from: accounts[1]});
+      assert(false)
+    } catch(err) {
+      assert(err)
+    }
   });
 
   //Uncomment the following test and it should fail. If it doesn't, there is a bug!
