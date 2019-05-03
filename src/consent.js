d58ddcfc10642117abdc2892aaae1c2c063c686e
Michael Smith <mas9311@gmail.com>
2019-05-02 21:28:56 -0600

    Updated the contracts address and removed comments

--- src/consent.js
+++ src/consent.js
@@ -1,6 +1,6 @@
 import web3 from "./web3";
 
 const abi = require("./compiled/ConsentContract")["abi"];
-const address = "0xe890E5759170f3332404a7Df8a0b625B5b439c25"; // final contract address
+const address = "0x01278d6f81a1EDfa03d205E7d54546E42F5e573e"; // final contract address
 
 export default new web3.eth.Contract(abi, address);
